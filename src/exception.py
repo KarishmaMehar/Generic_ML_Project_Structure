94c48b1252cef3c263e7cca0e54f2a83c6afaf3b
KarishmaMehar <karishma.razi1094@gmail.com>
2023-03-10 12:53:06 +0530

    logging & exceptio

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,6 @@
 import sys
-import logging
+from logger import logging
+
 
 def error_message_detail(error, error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()
@@ -15,13 +16,13 @@ class CustomException(Exception):
         self.error_message = error_message_detail(error_message, error_detail=error_detail)
 
     def __str__(self):
-        return self.error_message.__str__()
+        return self.error_message
 
 """
 if __name__ == "__main__" :
     try:
         a = 1/0
-    except ZeroDivisionError as e:
-        #logging.info("Devide by zero error")
+    except Exception as e:
+        logging.info("Devide by zero error")
         raise CustomException(e,sys) 
-      """
+"""      
